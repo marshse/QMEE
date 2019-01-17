a12ee91d4256accc2891135e53878224e7c7ce26
Susan Marsh-Rollo <marshse@mcmaster.ca>
2019-01-16 22:34:00 -0500

    Learning the environment still and how to navigate the levels

---
+++ assignment_week_2/assignment_week_jan14.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+
+## Import the data
+fish_info_dat <- read.csv("assignment_week_2/Corse_2016_Genetics_book.csv")
+
+## Examine the structure
+str(fish_info_dat)
+
+summary(fish_info_dat)
+
+## Look for mistakes
+
+
+## Make 1-2 plots to look for errors or anomalies
+
+
+
+
