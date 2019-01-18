782b10f9bdcd61816c28f7e95d15aaeadd53a29b
Susan Marsh-Rollo <marshse@mcmaster.ca>
2019-01-18 13:16:32 -0500

    Friday deadline assignment submission

--- assignment_week_2/assignment_week_jan14.R
+++ assignment_week_2/assignment_week_jan14.R
@@ -5,42 +5,46 @@ library(g.data)
 ## Import the data
 fish_info_data <- read.csv("assignment_week_2/Corse_2016_Genetics_book.csv")
 
-## ASSIGNMENT: Examine the structure
+## ASSIGNMENT: Examine the structure and look for mistakes on class assignment!
 str(fish_info_data)
+summary(fish_info_data)
+
 
 ## ASSIGNMENT: Examine data for mistakes
+# I examined and everything has the proper class assignment and 
+# no errors generated in the summary.
 
-# Remove the NA's  ????
 
+# Select data I want to plot (Type, SL, Wt)
+# Need to exlude Type(rostratus and Sat/sn)! I can't get this to work yet.
+fish_info_data2 <- fish_info_data %>% 
+  # select(Type = NM, Sat, sn, f) %>% 
+  select(Type, SL, Wt)
 
-# Remove data not needed for summary
-# Need to get rid of Types(rostratus and Sat/sn)!
-fish_info_data2 <- fish_info_data
-
-# Get summary
+# Re-examine summary after selecting columns
 summary(fish_info_data2)
 
-#Change names of vectors (f to Fem, sn to SnM, Sat to SatM)
-fish_info_data3 <- fish_info_data2
-
-# Get summary to check this has been changed
-summary(fish_info_data3)
 
 ## ASSIGNMENT: Make 1-2 plots to look for errors or anomalies
 
 # Plot the Wt (weight) of fish by type (Fem, NM, SatM,SnM)
 
-fish_info_dat3 <- select(fish_info_dat3, Type, Wt)
- 
-ggplot(data = fish_info_dat3, mapping = aes(x = Type, y = Wt)) +
+ggplot(data = fish_info_data2, mapping = aes(x = Type, y = Wt)) +
   geom_boxplot()
   
 #  PLot the SL (standard length) of fish by type (Fem, NM, SatM,SnM)
 
-fish_info_dat3 <- select(fish_info_dat3, Type, SL)
-
-ggplot(data = fish_info_dat3, mapping = aes(x = Type, y = SL)) +
+ggplot(data = fish_info_data2, mapping = aes(x = Type, y = SL)) +
   geom_boxplot()
 
 
+##  I AM ALSO STILL WORKING ON FIGURING OUT HOW TO DO THIS:
+## Change names of vectors (f to Fem, sn to SnM, Sat to SatM in TYPES)
+#fish_info_data3 <- fish_info_data2
+#sex <- fish_info_data2$sex
+#head(sex)
+#levels(sex)
+
+# Get summary to check Type names has been changed
+#summary(fish_info_data3)
 
