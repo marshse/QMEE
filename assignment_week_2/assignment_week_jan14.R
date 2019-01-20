1442f942b03c6a3194545d8648571d963bc3f0f0
Susan Marsh-Rollo <marshse@mcmaster.ca>
2019-01-20 18:02:43 -0500

    Figured out how to change x-axis labels.  Still could not filter out certain rows in column "Type"

--- assignment_week_2/assignment_week_jan14.R
+++ assignment_week_2/assignment_week_jan14.R
@@ -6,8 +6,8 @@ library(g.data)
 fish_info_data <- read.csv("assignment_week_2/Corse_2016_Genetics_book.csv")
 
 ## ASSIGNMENT: Examine the structure and look for mistakes on class assignment!
-str(fish_info_data)
-summary(fish_info_data)
+# str(fish_info_data)
+# summary(fish_info_data)
 
 ## ASSIGNMENT: Examine data for mistakes
 # I examined and everything has the proper class assignment and 
@@ -16,39 +16,36 @@ summary(fish_info_data)
 
 # Select data I want to plot (Type, SL, Wt)
 # Need to exlude Type(rostratus and Sat/sn)! I can't get this to work yet.
+## If I take out the comment here I get only some of the rows I want. Got only 19 obs.
 
 fish_info_data2 <- fish_info_data %>% 
-#  filter(fish_info_data, Type == c("NM", "Sat", "sn", "f")) %>% 
+#  filter(Type == c("NM", "Sat", "sn", "f")) %>% 
   select(Type, SL, Wt)
-
-## If I do it this way why is this excluding some of the vectors I want? Got 19 obs.
-# fish_info_data2 <- filter(fish_info_data, Type == c("NM", "Sat", "sn", "f")) %>% 
-#  select(Type, SL, Wt)
 
 
 # Re-examine summary after selecting columns
-summary(fish_info_data2)
+# summary(fish_info_data2)
 
 
 ## ASSIGNMENT: Make 1-2 plots to look for errors or anomalies
 
-# Plot the Wt (weight) of fish by type (Fem, NM, SatM,SnM)
+# I want to rename my x axis labels. This shows me the order of x axis labels by Type
+# levels(fish_info_data2$Type)
+
+levels(fish_info_data2$Type)[1:6] <- c("Female", "NM","rostratus", "SatM","Sat/sn", "SnM")
+
+
+# Plot the Wt (weight) of fish by type (Fem, NM, SatM, SnM)
 
 ggplot(data = fish_info_data2, mapping = aes(x = Type, y = Wt)) +
   geom_boxplot()
   
+# But I have introduced something weird here for the rostratus. No value at ~5.3!
+
+
 #  PLot the SL (standard length) of fish by type (Fem, NM, SatM,SnM)
 
 ggplot(data = fish_info_data2, mapping = aes(x = Type, y = SL)) +
   geom_boxplot()
 
 
-##  I AM ALSO STILL WORKING ON FIGURING OUT HOW TO DO THIS:
-## Change names of vectors (f to Fem, sn to SnM, Sat to SatM in TYPES)
-#fish_info_data3 <- fish_info_data2
-#sex <- fish_info_data2$sex
-#head(sex)
-#levels(sex)
-
-# Get summary to check Type names has been changed
-#summary(fish_info_data3)
