50719d980d7e9bedf58ecab3a4fb8c6877ec632c
Susan Marsh-Rollo <marshse@mcmaster.ca>
2019-01-27 18:28:30 -0500

    Getting ready to submit assignment

--- assignments/assignment_week_21_Jan_b.R
+++ assignments/assignment_week_21_Jan_b.R
@@ -1,29 +1,25 @@
 library(tidyverse)
 library(readr)
+library(ggsci)
+library(lubridate)
 
-# I think I don't need to load this if I have already loaded tidyverse?
-# library(ggplot2)
-
-
-## ASSIGNMENT: Construct some (more!) ggplots using my data :-)
+## ASSIGNMENT_21_Jan: Construct some (more!) ggplots using my data :-)
 
 ## Import the data
-fish_info_data <- read.csv("Corse_2016_Genetics_book.csv")
+fish_info_data <- read_csv("Corse_2016_Genetics_book.csv")
 
-# Select data I want to plot (Type, SL, Wt)
+
+# Select data I want to use (Type, SL, Wt)
 # Need to exlude Type(rostratus and Sat/sn)
 fish_info_data2 <- fish_info_data %>% 
-  filter(Type %in% c("NM", "Sat", "sn", "f")) %>% 
+  filter(!(Type %in% c("rostratus","Sat/sn"))) %>% 
+  droplevels() %>% 
   select(Type, SL, Wt)
-
-# I want to rename my x axis labels. This shows me the order of x axis labels by Type
-# levels(fish_info_data2$Type)
-levels(fish_info_data2$Type)[1:6] <- c("Female", "NM","rostratus", "SatM","Sat/sn", "SnM")
 
 
 #  Create Body Condition column
 fish_info_data3 <- fish_info_data2 %>% 
-  mutate(body_condition = 100 * (Wt/(10*SL)^3))
+  mutate(body_condition = 100000000 * (Wt/(10*SL)^3))
 
 
 # Look at distibution of Wt, SL and Body Condition by Type (Fem, NM, SatM, SnM) using 
@@ -48,21 +44,42 @@ ggplot(data = fish_info_data3, mapping = aes(x = Type, y = body_condition, colou
   geom_point()
 
 
-# How much does body condition decrease for each Type over the spawning season (Date?)
+# Playing around to see how I can add geom commands together and change parameters :-)
+ggplot(data = fish_info_data3, mapping = aes(x = Type, y = SL, colour=Type)) +
+  geom_boxplot() +
+  geom_dotplot(binaxis = 'y', stackdir = 'center', dotsize = .5) +
+  geom_jitter(shape= 10, position = position_jitter(0.2))
+
+
+# Look at relationship for Wt and SL in different Types
+
+ggplot(fish_info_data5, aes(y = Wt, x = SL, colour = Type)) +
+  geom_point() +
+  theme_bw() + scale_color_hue()
+
+
+# Does body condition decrease for each Type over the spawning season (Date?)
 
 fish_info_data4 <- fish_info_data %>% 
-  filter(Type %in% c("NM", "Sat", "sn", "f")) %>% 
+  filter(!(Type %in% c("rostratus","Sat/sn"))) %>% 
+  droplevels() %>% 
   select(Type, SL, Wt, Date)
 
 fish_info_data5 <- fish_info_data4 %>% 
-  mutate(body_condition = 100 * (Wt/(10*SL)^3))
+  mutate(body_condition = 100000000 * (Wt/(10*SL)^3))
 
 ggplot(data = fish_info_data5,mapping = aes(x = Date, y = body_condition, colour=Type)) + 
-  geom_point()
+  geom_point() +
+  theme_bw() + scale_color_futurama()
 
 
-# TO DO!
+# TO WORK THROUGH STILL!
+
 # Need to clean up dates so that if displays in the proper order
-# Draw lines between each of the Types across the dates? General line?
-# trying to upload to GitHub
+# Working with lubridate to figure this out:  
+# stamp("20-May") (D)
+
+# Draw lines between each of the Types across the dates for last plot?
+
+
 
