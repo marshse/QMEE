3562023b8e5857d7ea25a78bfdf242301a249f36
Susan Marsh-Rollo <marshse@mcmaster.ca>
2019-02-06 19:13:15 -0500

    Adding new files for week_4_Feb assignments

---
+++ assignments/assignment_week_4_Feb.R
@@ -0,0 +1,6 @@
+## Formulate two different hypotheses about your data, and describe how you would 
+## test them with two different permutation tests (In README file)
+## Implement one or both of these tests in R. 
+## You can use permutation, or you can use a classic test if you explain clearly 
+## how it corresponds to a permutation test. Best would be to use both.
+
