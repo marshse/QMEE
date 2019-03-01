2f284a0c3c0dceb0785fd34e124e3a1021743b42
Susan Marsh-Rollo <marshse@mcmaster.ca>
2019-02-28 21:01:04 -0500

    Working through assignment for week 7

--- assignments/assignment_7_week_25_Feb.R
+++ assignments/assignment_7_week_25_Feb.R
@@ -4,24 +4,48 @@
 
 # Print from Class Example:
 
+library(tidyverse)
 library(R2jags)
 library(readr)
-library(ggplot2) 
+library(ggplot2); theme_set(theme_bw())
 
-### STOPPED HERE
-df <- read_csv("http://biostat.mc.vanderbilt.edu/wiki/pub/Main/DataSets/FEV.csv")
+library("coda")
+library("emdbook")    ## for lump.mcmc.list() and as.mcmc.bugs()
+library("arm")        ## for coefplot, bayesglm
+library("lattice")
+library("rstanarm")
+library("dotwhisker")
+library("broom.mixed")
 
-print(ggplot(df, aes(x=height, y=fev))
+df <- read_csv("Genetics book Corse 2018.csv")
+
+# Create a function that I can use to generate different subsets of the data
+# and to calculate body condition
+proc_fun <- function(data,types) {
+  return(data
+         %>%  filter(Type %in% types)
+         %>% droplevels()
+         %>% select(Type, SL, Wt, Date)
+         %>% mutate(body_condition = 1e8 * (Wt/(10*SL)^3))
+         %>% drop_na())
+}
+fi_d3 <- df %>% proc_fun(types=c("NM","Sat","sn"))  
+
+
+print(ggplot(fi_d3, aes(x=Type, y=body_condition))
       + geom_point()
       + geom_smooth()
 )
 
-linmod <- lm(fev~height, data=df)
+linmod <- lm(Type~body_condition, data=fi_d3)
 plot(linmod)
 
-N <- nrow(df)
+N <- nrow(fi_d3)
 
-bayesmod <- with(df, jags(model.file='fev.bug'
+## Still not sure that I am setting up the linear model appropriately...
+
+### STOPPED HERE
+bayesmod <- with(fi_d3, jags(model.file='fev.bug'
                           , parameters=c("b_height", "b_0", "tau")
                           , data = list('fev' = fev, 'height' = height, 'N'=N)
                           , n.chains = 4
