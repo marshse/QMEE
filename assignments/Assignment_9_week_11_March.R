79eb22f897b6482f739451dbfc8812838f4a47a4
Susan Marsh-Rollo <marshse@mcmaster.ca>
2019-03-17 22:10:58 -0400

    Ah...working on Assignment 9

---
+++ assignments/Assignment_9_week_11_March.R
@@ -0,0 +1,12 @@
+Assignment_9_week_11_March
+
+# Make a (possibly generalized) linear mixed model for one or more of your hypotheses.
+# Discuss what choices you made about mixed effects (both grouping variables and related 
+##  effects; i.e., what did you put on each side of a pipe)
+# Identify the maximal model that you could have used, and explain what you left out 
+## (if anything) and why
+# Discuss your results
+
+# Set up model
+
+# Estimate parameters
